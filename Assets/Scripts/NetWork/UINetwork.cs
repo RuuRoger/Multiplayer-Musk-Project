9c2f6a35b61da10674d5ddd951fcb2e2e95bc1d0
Roger Lozano <lozanofierroruu@gmail.com>
2026-01-08 06:07:16 +0100

    feat: add script for UI to start in server

---
+++ Assets/Scripts/NetWork/UINetwork.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.NetWork
+{
+    public class UINetwork : MonoBehaviour
+    {
+        // ================================================== FIELDS ==================================================
+        [SerializeField] private GameObject m_connecitngPanel;
+    }
+}
